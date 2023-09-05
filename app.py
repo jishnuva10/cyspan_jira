3e0992e6e6075dbbe3e794d6de9611324ec37dcc
jishnuva10 <117532784+jishnuva10@users.noreply.github.com>
2023-09-05 15:29:28 +0530

    Update app.py
    auto mail system enabled

--- app.py
+++ app.py
@@ -56,3 +56,78 @@ st.download_button(
     file_name='status.csv',
     mime='text/csv',
 )
+
+def automail():
+
+    import email, smtplib, ssl
+    import time
+
+    from email import encoders
+    from email.mime.base import MIMEBase
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+
+
+    sender_email = 'cyspanautomail@gmail.com'
+    password = 'vgitaridhtqsxahi'
+    #receiver_email = "jishnuva10@gmail.com"
+    receiver_email = 'pbabu@integretz.com'
+    subject = "Jira Status"
+
+
+    from pretty_html_table import build_table
+    output = build_table(status, 'blue_light')
+
+
+
+
+    body = "Hi,\n\nPlease find attached status for your reference.\n\n"
+
+    body2 = "\n\nThanks and Regards\n\nJishnu V A\n+91 9995355951\n\n"
+
+
+    html = """\
+    <html>
+      <head></head>
+      <body>
+        {0}
+      </body>
+    </html>
+    """.format(status.to_html())
+
+    part1 = MIMEText(html, 'html')
+
+
+
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = "jishnuva10@gmail.com"
+    message["Subject"] = subject
+    message["Cc"] = "cyspanautomail@gmail.com"
+
+
+
+    # Add body to email
+    message.attach(MIMEText(body, "plain"))
+    message.attach(part1)
+    message.attach(MIMEText(body2, "plain"))
+
+
+
+    # Add attachment to message and convert message to string
+    #message.attach(part1)
+    text = message.as_string()
+
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, text)
+    print("mail send")
+
+
+st.button('Email status',automail())
+
+
+
